b3b332144b52f3b12abee2800c53aac471102835
NVorobev13 <n.a.vorobev13@gmail.com>
2022-07-07 20:26:01 +0300

    created unittest for Chernovik2.py

--- test_Chernovik.py
+++ test_Chernovik.py
@@ -1,7 +1,16 @@
 from unittest
 import unittest
-
-
-class TestText(unittest.TestCase):
-    
-    def 
+from Calculator import Calculator
+class Caclulator(unittest.TestCase)
+    def setUp(self):
+        self.calculator = Calculator()
+    def test_add(self):
+        self.assertEqual(self.calculator.add(7, 3) 10)
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(7, 3) 21)
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(7, 3) 4)
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(21, 3) 7)    
+if __name__ == "__main__"
+    unittest.main()
