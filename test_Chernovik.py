8abe331f9311cdbffa7d1ef0d35915dead64b6a1
NVorobev13 <n.a.vorobev13@gmail.com>
2022-07-07 21:37:50 +0300

    created Calculator.py

--- test_Chernovik.py
+++ test_Chernovik.py
@@ -1,15 +1,15 @@
 import unittest
-from Calculator import Calculator
-class Caclulator(unittest.TestCase):
+from Chernovik2 import Chernovik2
+class Chernovik2(unittest.TestCase):
     def setUp(self):
-        self.calculator = Calculator()
+        self.Chernovik2 = Chernovik2()
     def test_add(self):
-        self.assertEqual(self.calculator.add(7, 3), 10)
+        self.assertEqual(self.Chernovik2.add(7, 3), 10)
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(7, 3), 21)
+        self.assertEqual(self.Chernovik2.multiply(7, 3), 21)
     def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(7, 3), 4)
+        self.assertEqual(self.Chernovik2.subtract(7, 3), 4)
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(21, 3), 7)    
+        self.assertEqual(self.Chernovik2.divide(21, 3), 7)    
 if __name__ == "__main__":
     unittest.main()
