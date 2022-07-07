1b186b090eb619beb9a459b1fdde66fc2d21a41f
NVorobev13 <n.a.vorobev13@gmail.com>
2022-07-07 21:15:33 +0300

    added test_Chernovik.py

--- test_Chernovik.py
+++ test_Chernovik.py
@@ -1,16 +1,15 @@
-from unittest
 import unittest
 from Calculator import Calculator
-class Caclulator(unittest.TestCase)
+class Caclulator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
     def test_add(self):
-        self.assertEqual(self.calculator.add(7, 3) 10)
+        self.assertEqual(self.calculator.add(7, 3), 10)
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(7, 3) 21)
+        self.assertEqual(self.calculator.multiply(7, 3), 21)
     def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(7, 3) 4)
+        self.assertEqual(self.calculator.subtract(7, 3), 4)
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(21, 3) 7)    
-if __name__ == "__main__"
+        self.assertEqual(self.calculator.divide(21, 3), 7)    
+if __name__ == "__main__":
     unittest.main()
