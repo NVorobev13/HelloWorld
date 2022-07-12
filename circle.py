ec0e33cdbd017414b1e4cf416040d6062b2e9830
NVorobev13 <n.a.vorobev13@gmail.com>
2022-07-12 23:38:09 +0300

    update circle.py

--- circle.py
+++ circle.py
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
 from math import pi
-=======
-from email import message
-from math import perm, radians
->>>>>>> dev
 
 def circle_area(radius):
     if type(radius) not in [int, float]:
         raise TypeError("Radius must be non-negative real number only")
     if radius < 0:
-<<<<<<< HEAD
         raise ValueError("Radius cant't be negative")
     return pi*radius**2
-=======
-        raise ValueError("Радиус не может быть отрицательный")
-    return pi*radius**2
-
-
-# r_line = [2, -3, 2 + 3j, True, [2], 'seven']
-# message = 'Площадь окружности с радиусом {radius} --> {area}'
-# 
-# for r in r_line:
-#     s = circle_area(r)
-#     print(message.format(radius = r, area = s))
->>>>>>> dev
